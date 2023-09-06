d181ce698844f0d162a44b47c65a35a19aeaa8da
Kevin Dohmen <kevindohmen06@gmail.com>
2023-09-06 13:10:44 +0200

    Basic site (instabiel)

---
+++ js/master.js
@@ -0,0 +1,7 @@
+function sb_open() {
+    document.getElementById("sideBar").style.display = "block";
+  }
+  
+  function sb_close() {
+    document.getElementById("sideBar").style.display = "none";
+  }
