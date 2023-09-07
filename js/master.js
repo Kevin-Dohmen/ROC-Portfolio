73b06bc05b8264a217728092f0fa85e87cf96bd9
Kevin Dohmen <kevindohmen06@gmail.com>
2023-09-07 16:03:37 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -1,7 +1,9 @@
 function sb_open() {
     document.getElementById("sideBar").style.display = "block";
+    document.getElementById("menuOpen").style.display = "none";
   }
   
   function sb_close() {
     document.getElementById("sideBar").style.display = "none";
+    document.getElementById("menuOpen").style.display = "block";
   }
